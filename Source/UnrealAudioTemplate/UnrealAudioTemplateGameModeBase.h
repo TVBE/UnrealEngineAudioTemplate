adb163c6504d1f6f24770fc2458c4f9c39c44b0e
Tim Verberne <timverbe@gmail.com>
2023-01-08 10:07:45 +0100

    project setup

---
+++ Source/UnrealAudioTemplate/UnrealAudioTemplateGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealAudioTemplateGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALAUDIOTEMPLATE_API AUnrealAudioTemplateGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
